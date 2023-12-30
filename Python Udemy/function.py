a44c13ff3f3e570e542bcdf27511b6969102268d
Vishal Chaurasia <vishal17500@gmail.com>
2023-12-30 15:52:38 +0530

    commit

---
+++ Python Udemy/function.py
@@ -0,0 +1,17 @@
+def function_name():
+    print(2+2)
+
+def function(a):
+    print(a+2)
+
+s1="The number "
+def func(p1,p2,p3):
+    print(p1+str(2)+p3) 
+
+def default(num1=7,num2=8):
+    return num1*num2
+print(default()+44)
+
+function_name()
+function(10)
+func(s1,5," is an integer.")
